9f5535b37c0989ba95ebd64c5a893d5b7b6ce96d
KevinMora04 <39573209+kevinmora47@users.noreply.github.com>
2019-04-19 21:31:00 -0600

    Initial commit.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express = require('express');//Importamos el framework para su uso.
+const morgan = require('morgan');
 const app = express();//Este es mi servidor.
+
 
 //Configuraciones
 app.set('port', process.env.PORT || 3000);//Setiamos nuestro puerto, en este caso el S.O eligira el puerto o usara por defecto el 3000.
 
+//MiddleWares
+app.use(morgan('dev'));
+app.use(express.json());
+
+//Rutas
+app.use('/api/tasks', require('./routes/task_routes'));
 
 
 
-app.listen(app.get('port'),()=>{//Creacion de
+app.listen(app.get('port'),()=>{//Creacion de nuestro puerto
    
     console.log(`Servidor en puerto ${app.get('port')}`);
     
